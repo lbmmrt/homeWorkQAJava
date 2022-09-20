8961cea3668d291abf9c62fe4436ac90aedc238b
Karina Palanchuk <58773933+lbmmrt@users.noreply.github.com>
2022-09-20 12:17:11 +0300

    Sixth lecture (#6)
    * sixth lecture
    
    * sixth lecture

---
+++ src/main/java/lesson6/Main.java
@@ -0,0 +1,20 @@
+package lesson6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+  public static void main(String[] args) {
+    List<Student> students = new ArrayList<>();
+
+    students.add(new Student(2, "Ivan", "A", Map.of("Math", 3, "Fisics", 2, "Rus", 5)));
+    students.add(new Student(2, "Alex", "A", Map.of("Math", 2, "Fisics", 2, "Rus", 2)));
+    students.add(new Student(2, "Petr", "A", Map.of("Math", 2, "Fisics", 2, "Rus", 5)));
+    students.add(new Student(1, "Masha", "A", Map.of("Math", 3, "Fisics", 4, "Rus", 4)));
+
+    System.out.println(FirstTask.filterStudents(students));
+
+    FirstTask.findStudentWithSelectedCourse(students, 2);
+  }
+}
